f52a9daaffcff87e545d790770bf3452110189fa
kzmaro <maronkazu94@gmail.com>
2019-12-30 18:13:28 +0900

    ch5 PCA p149まで

--- ch5/pca.py
+++ ch5/pca.py
@@ -4,6 +4,9 @@ from sklearn.model_selection import train_test_split
 # from sklearn.cross_validationは書籍のミス??存在しない。
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from sklearn.linear_model import LogisticRegression
+from sklearn.decomposition import PCA
 
 df_wine = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data'
                       , header=None)
@@ -66,3 +69,67 @@ w = np.hstack((eigen_pairs[0][1][:, np.newaxis], eigen_pairs[1][1][:, np.newaxis
 print('Matrix W:\n', w)
 
 # 次はp147~
+X_train_pca = X_train_std.dot(w)
+
+colors = ['r', 'b', 'g']
+markers = ['s', 'x', 'o']
+
+# クラスラベル、点の色、点の種類の組み合わせからなるリストを生成してプロットする
+# lと1の違いに注意する！！
+for l, c, m in zip(np.unique(y_train), colors, markers):
+    plt.scatter(X_train_pca[y_train == l, 0], X_train_pca[y_train == l, 1], c=c,
+                label=1, marker=m)
+
+plt.xlabel('PC 1')
+plt.ylabel('PC 2')
+plt.legend(loc='lower left')
+plt.tight_layout()
+plt.show()
+
+
+# 5.1.5 scikit-learnの主成分分析
+
+def plot_decision_regions(X, y, classifier, resolution=0.02):
+    # マーカーとカラーマップの準備
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # 決定領域のプロット
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+
+    # グリッドポイントの生成
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+
+    # 各特徴量を1次元配列に変換して予測を実行
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+
+    # 予測結果の元のグリッドポイントのデータサイズに変換
+    Z = Z.reshape(xx1.shape)
+    # グリッドポイントの等高線のプロット
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # クラスごとにサンプルをプロット
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1], alpha=0.6, c=cmap(idx),
+                    edgecolor='black', marker=markers[idx], label=cl)
+
+
+pca = PCA(n_components=2)
+lr = LogisticRegression()
+
+X_train_pca = pca.fit_transform(X_train_std)
+X_test_pca = pca.transform(X_test_std)
+# トレーニングデータでロジスティック回帰を実行
+lr.fit(X_train_pca, y_train)
+plot_decision_regions(X_train_pca, y_train, classifier=lr)
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.legend(loc='lower left')
+plt.tight_layout()
+plt.show()
